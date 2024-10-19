cd020e23a1d1cef61f3e7985294193ea8a363034
mjayio <mjdeveng@gmail.com>
2024-10-19 09:23:11 -0400

    Refactor textnode.py and test_textnode.py

--- src/helpers.py
+++ src/helpers.py
@@ -1,5 +1,6 @@
 from textnode import TextNode, TextType
 from typing import List
+import re
 
 textType_mappings = {
     "text": TextType.TEXT,
@@ -26,14 +27,167 @@ def split_nodes_delimiter(old_nodes: List[TextNode], delimiter: str, text_type: 
     [TextNode]: A list of new text nodes with alternating text types.
 
     Example:
-    >>> nodes = [TextNode("Hello, World!", TextType.TEXT)]
-    >>> split_nodes_delimiter(nodes, ",", TextType.BOLD)
-    [TextNode("Hello", TextType.TEXT), TextNode(" World!", TextType.BOLD)]
+    >>> node = TextNode("This is text with a `code block` word", TextType.TEXT)
+    >>> new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+    [
+        TextNode("This is text with a ", TextType.TEXT),
+        TextNode("code block", TextType.CODE),
+        TextNode(" word", TextType.TEXT),
+    ]
     """
     new_nodes = []
     for node in old_nodes:
+        # if re.search(r"\ +" + re.escape(delimiter) + r"+" + re.escape(delimiter) + r"\ +", node.text) is None:
+        #     new_nodes.append(node)
+        #     continue
         parts = node.text.split(delimiter)
         for i, part in enumerate(parts):
             current_text_type = textType_mappings[node.text_type] if i % 2 == 0 else text_type
             new_nodes.append(TextNode(part, current_text_type, node.url if i % 2 == 0 else None))
     return new_nodes
+
+def extract_markdown_images(text: str) -> List[tuple]:
+    """
+    Extracts all markdown image references from the given text.
+
+    Args:
+        text (str): The input text containing markdown image references.
+
+    Returns:
+        list of tuple: A list of tuples where each tuple contains the alt text and the URL of an image.
+
+    Example:
+    >>> text = "Here is an image ![alt text](http://example.com/image.jpg)"
+    >>> extract_markdown_images(text)
+    [('alt text', 'http://example.com/image.jpg')]
+    """
+    matches = re.findall(r"!\[(.*?)\]\((.*?)\)", text)
+    return matches
+
+def extract_markdown_links(text: str) -> List[tuple]:
+    """
+    Extracts all markdown link references from the given text.
+
+    Args:
+        text (str): The input text containing markdown link references.
+
+    Returns:
+        list of tuple: A list of tuples where each tuple contains the link text and the URL of a link.
+
+    Example:
+    >>> text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+    >>> extract_markdown_links(text)
+    [("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")]
+    """
+    matches = re.findall(r"\[(.*?)\]\((.*?)\)", text)
+    return matches
+
+
+def split_nodes_image(old_nodes: List[TextNode]) -> List[TextNode]:
+    """
+    Splits nodes containing markdown images into separate text and image nodes.
+
+    Args:
+        old_nodes (list): A list of nodes, where each node has a 'text' attribute 
+        containing markdown content.
+
+    Returns:
+        list: A list of nodes where markdown images are split into separate 
+        TextNode objects. TextNode objects have a type attribute indicating 
+        whether they are text or image nodes.
+
+    Example:
+        Given a node with text "Here is an image ![alt text](url)", the function will 
+        return three nodes: one with text "Here is an image ", one with the image alt text, 
+        and one with text after the image.
+    """
+    nodes = []
+    for node in old_nodes:
+        images = extract_markdown_images(node.text)
+        prev_end = 0
+        if len(images) == 0:
+            nodes.append(node)
+        else:
+            matches = re.finditer(r"!\[.*?\]\(.*?\)", node.text)
+            for i, match in enumerate(matches):
+                start, end = match.span()
+                if start > 0:
+                    nodes.append(TextNode(node.text[prev_end:start], TextType.TEXT))
+                nodes.append(TextNode(images[i][0], TextType.IMAGE, images[i][1]))
+                prev_end = end
+            if prev_end < len(node.text):
+                nodes.append(TextNode(node.text[prev_end:], TextType.TEXT))
+    return nodes
+
+def split_nodes_link(old_nodes: List[TextNode]) -> List[TextNode]:
+    """
+    Splits nodes containing markdown links into separate text and link nodes.
+
+    Args:
+        old_nodes (list): A list of nodes, where each node has a 'text' attribute 
+        containing markdown content.
+
+    Returns:
+        list: A list of nodes where markdown links are split into separate 
+        TextNode objects. TextNode objects have a type attribute indicating 
+        whether they are text or link nodes.
+
+    Example:
+        Given a node with text "This is a [link](url)", the function will 
+        return three nodes: one with text "This is a ", one with the link text, 
+        and one with text after the link.
+    """
+    nodes = []
+    for node in old_nodes:
+        links = extract_markdown_links(node.text)
+        prev_end = 0
+        if len(links) == 0:
+            nodes.append(node)
+        else:
+            matches = re.finditer(r"\[.*?\]\(.*?\)", node.text)
+            for i, match in enumerate(matches):
+                start, end = match.span()
+                if start > 0:
+                    nodes.append(TextNode(node.text[prev_end:start], TextType.TEXT))
+                nodes.append(TextNode(links[i][0], TextType.LINK, links[i][1]))
+                prev_end = end
+            if prev_end < len(node.text):
+                nodes.append(TextNode(node.text[prev_end:], TextType.TEXT))
+    return nodes
+
+
+def text_to_textnodes(text: str) -> List[TextNode]:
+    """
+    Converts a text string into a list of TextNode objects, splitting the text
+    based on various markdown delimiters and creating nodes with appropriate
+    text types.
+
+    Args:
+        text (str): The input text containing markdown content.
+
+    Returns:
+        List[TextNode]: A list of TextNode objects with appropriate text types.
+
+    Example:
+        >>> text = "This is **bold** and `code` and *italic* text with ![alt](image.png) and [link](url)"
+        >>> nodes = text_to_textnodes(text)
+        [
+            TextNode("This is ", TextType.TEXT),
+            TextNode("bold", TextType.BOLD),
+            TextNode(" and ", TextType.TEXT),
+            TextNode("code", TextType.CODE),
+            TextNode(" and ", TextType.TEXT),
+            TextNode("italic", TextType.ITALIC),
+            TextNode(" text with ", TextType.TEXT),
+            TextNode("alt", TextType.IMAGE, "image.png"),
+            TextNode(" and ", TextType.TEXT),
+            TextNode("link", TextType.LINK, "url"),
+        ]
+    """
+    node = TextNode(text, TextType.TEXT)
+    nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "*", TextType.ITALIC)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    return nodes
--- src/test_helpers.py
+++ src/test_helpers.py
@@ -1,15 +1,6 @@
 import unittest
 from textnode import TextNode, TextType
-from helpers import split_nodes_delimiter
-
-# Mapping of TextType to their respective delimiters
-delimiters = {
-    TextType.BOLD: "**",
-    TextType.ITALIC: "*",
-    TextType.CODE: "`",
-    TextType.LINK: ":",
-    TextType.IMAGE: "!"
-}
+from helpers import extract_markdown_images, extract_markdown_links, split_nodes_delimiter, split_nodes_image, split_nodes_link, text_to_textnodes
 
 class TestSplitNodesDelimiter(unittest.TestCase):
 
@@ -83,5 +74,313 @@ class TestSplitNodesDelimiter(unittest.TestCase):
         self.assertEqual(result, expected)
 
 
+class TestExtractMarkdownImages(unittest.TestCase):
+    
+    def test_single_image(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif)"
+        expected = [("rick roll", "https://i.imgur.com/aKaOqIh.gif")]
+        result = extract_markdown_images(text)
+        self.assertEqual(result, expected, "Single image extraction failed result was " + str(result) + " expected was " + str(expected))
+    
+    def test_multiple_images(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        expected = [("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")]
+        result = extract_markdown_images(text)
+        self.assertEqual(result, expected)
+
+    def test_no_images(self):
+        text = "This is text with no images"
+        expected = []
+        result = extract_markdown_images(text)
+        self.assertEqual(result, expected)
+
+    def test_image_with_no_alt_text(self):
+        text = "This is text with an image ![](https://i.imgur.com/aKaOqIh.gif)"
+        expected = [("", "https://i.imgur.com/aKaOqIh.gif")]
+        result = extract_markdown_images(text)
+        self.assertEqual(result, expected)
+
+    def test_image_with_special_characters_in_alt_text(self):
+        text = "This is text with an image ![alt text with special characters !@#$%^&*()](https://i.imgur.com/aKaOqIh.gif)"
+        expected = [("alt text with special characters !@#$%^&*()", "https://i.imgur.com/aKaOqIh.gif")]
+        result = extract_markdown_images(text)
+        self.assertEqual(result, expected)
+
+    def test_image_with_spaces_in_url(self):
+        text = "This is text with an image ![alt text](https://i.imgur.com/aKaOqIh%20with%20spaces.gif)"
+        expected = [("alt text", "https://i.imgur.com/aKaOqIh%20with%20spaces.gif")]
+        result = extract_markdown_images(text)
+        self.assertEqual(result, expected)
+        
+class TestExtractMarkdownLinks(unittest.TestCase):
+    
+    def test_single_link(self):
+        text = "This is text with a [link](https://example.com)"
+        expected = [("link", "https://example.com")]
+        result = extract_markdown_links(text)
+        self.assertEqual(result, expected)
+    
+    def test_multiple_links(self):
+        text = "This is text with a [link](https://example.com) and [another link](https://example.com/another)"
+        expected = [("link", "https://example.com"), ("another link", "https://example.com/another")]
+        result = extract_markdown_links(text)
+        self.assertEqual(result, expected)
+
+        
+class TestSplitNodesImage(unittest.TestCase):
+    
+    def test_single_image_org(self):
+        node = TextNode(
+            "This is text with an image ![to boot dev](https://www.boot.dev) and ![to youtube](https://www.youtube.com/@bootdotdev)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        expected = [
+            TextNode("This is text with an image ", TextType.TEXT),
+            TextNode("to boot dev", TextType.IMAGE, "https://www.boot.dev"),
+            TextNode(" and ", TextType.TEXT),
+            TextNode(
+                "to youtube", TextType.IMAGE, "https://www.youtube.com/@bootdotdev"
+            ),
+        ]
+        self.assertEqual(new_nodes, expected)
+
+    def test_single_image(self):
+        node = TextNode("This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif)", TextType.TEXT)
+        expected = [
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("rick roll", TextType.IMAGE, "https://i.imgur.com/aKaOqIh.gif")
+        ]
+        result = split_nodes_image([node])
+        self.assertEqual(result, expected)
+
+    def test_multiple_images(self):
+        node = TextNode("This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)", TextType.TEXT)
+        expected = [
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("rick roll", TextType.IMAGE, "https://i.imgur.com/aKaOqIh.gif"),
+            TextNode(" and ", TextType.TEXT),
+            TextNode("obi wan", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg")
+        ]
+        result = split_nodes_image([node])
+        self.assertEqual(result, expected)
+
+    def test_no_images(self):
+        node = TextNode("This is text with no images", TextType.TEXT)
+        expected = [node]
+        result = split_nodes_image([node])
+        self.assertEqual(result, expected)
+
+    def test_image_with_no_alt_text(self):
+        node = TextNode("This is text with an image ![](https://i.imgur.com/aKaOqIh.gif)", TextType.TEXT)
+        expected = [
+            TextNode("This is text with an image ", TextType.TEXT),
+            TextNode("", TextType.IMAGE, "https://i.imgur.com/aKaOqIh.gif")
+        ]
+        result = split_nodes_image([node])
+        self.assertEqual(result, expected)
+
+    def test_image_with_special_characters_in_alt_text(self):
+        node = TextNode("This is text with an image ![alt text with special characters !@#$%^&*()](https://i.imgur.com/aKaOqIh.gif)", TextType.TEXT)
+        expected = [
+            TextNode("This is text with an image ", TextType.TEXT),
+            TextNode("alt text with special characters !@#$%^&*()", TextType.IMAGE, "https://i.imgur.com/aKaOqIh.gif")
+        ]
+        result = split_nodes_image([node])
+        self.assertEqual(result, expected)
+
+    def test_image_with_spaces_in_url(self):
+        node = TextNode("This is text with an image ![alt text](https://i.imgur.com/aKaOqIh%20with%20spaces.gif)", TextType.TEXT)
+        expected = [
+            TextNode("This is text with an image ", TextType.TEXT),
+            TextNode("alt text", TextType.IMAGE, "https://i.imgur.com/aKaOqIh%20with%20spaces.gif")
+        ]
+        result = split_nodes_image([node])
+        self.assertEqual(result, expected)
+        
+class TestSplitNodesLink(unittest.TestCase):
+    
+    def test_single_link_org(self):
+        node = TextNode(
+            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_link([node])
+        expected = [
+            TextNode("This is text with a link ", TextType.TEXT),
+            TextNode("to boot dev", TextType.LINK, "https://www.boot.dev"),
+            TextNode(" and ", TextType.TEXT),
+            TextNode(
+                "to youtube", TextType.LINK, "https://www.youtube.com/@bootdotdev"
+            ),
+        ]
+        self.assertEqual(new_nodes, expected)
+
+    def test_single_link(self):
+        node = TextNode("This is text with a [link](https://example.com)", TextType.TEXT)
+        expected = [
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("link", TextType.LINK, "https://example.com")
+        ]
+        result = split_nodes_link([node])
+        self.assertEqual(result, expected)
+
+    def test_multiple_links(self):
+        node = TextNode("This is text with a [link](https://example.com) and [another link](https://example.com/another)", TextType.TEXT)
+        expected = [
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("link", TextType.LINK, "https://example.com"),
+            TextNode(" and ", TextType.TEXT),
+            TextNode("another link", TextType.LINK, "https://example.com/another")
+        ]
+        result = split_nodes_link([node])
+        self.assertEqual(result, expected)
+
+    def test_no_links(self):
+        node = TextNode("This is text with no links", TextType.TEXT)
+        expected = [node]
+        result = split_nodes_link([node])
+        self.assertEqual(result, expected)
+
+    def test_link_with_special_characters(self):
+        node = TextNode("This is text with a [link with special characters !@#$%^&*()](https://example.com)", TextType.TEXT)
+        expected = [
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("link with special characters !@#$%^&*()", TextType.LINK, "https://example.com")
+        ]
+        result = split_nodes_link([node])
+        self.assertEqual(result, expected)
+
+    def test_link_with_spaces_in_url(self):
+        node = TextNode("This is text with a [link](https://example.com/with%20spaces)", TextType.TEXT)
+        expected = [
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("link", TextType.LINK, "https://example.com/with%20spaces")
+        ]
+        result = split_nodes_link([node])
+        self.assertEqual(result, expected)
+
+class TestTextToTextNodes(unittest.TestCase):
+    
+    def test_text_to_textnodes(self):
+        text = "This is **text** with an *italic* word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        text_nodes = text_to_textnodes(text)
+        expected = [
+            TextNode("This is ", TextType.TEXT),
+            TextNode("text", TextType.BOLD),
+            TextNode(" with an ", TextType.TEXT),
+            TextNode("italic", TextType.ITALIC),
+            TextNode(" word and a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" and an ", TextType.TEXT),
+            TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+            TextNode(" and a ", TextType.TEXT),
+            TextNode("link", TextType.LINK, "https://boot.dev"),
+        ]
+        self.assertEqual(text_nodes, expected)
+
+    def test_text_with_no_special_formatting(self):
+        text = "This is plain text with no special formatting."
+        text_nodes = text_to_textnodes(text)
+        expected = [TextNode("This is plain text with no special formatting.", TextType.TEXT)]
+        self.assertEqual(text_nodes, expected)
+
+    def test_text_with_only_bold(self):
+        text = "This is **bold** text."
+        text_nodes = text_to_textnodes(text)
+        expected = [
+            TextNode("This is ", TextType.TEXT),
+            TextNode("bold", TextType.BOLD),
+            TextNode(" text.", TextType.TEXT),
+        ]
+        self.assertEqual(text_nodes, expected)
+
+    def test_text_with_only_italic(self):
+        text = "This is *italic* text."
+        text_nodes = text_to_textnodes(text)
+        expected = [
+            TextNode("This is ", TextType.TEXT),
+            TextNode("italic", TextType.ITALIC),
+            TextNode(" text.", TextType.TEXT),
+        ]
+        self.assertEqual(text_nodes, expected)
+
+    def test_text_with_only_code(self):
+        text = "This is `code` text."
+        text_nodes = text_to_textnodes(text)
+        expected = [
+            TextNode("This is ", TextType.TEXT),
+            TextNode("code", TextType.CODE),
+            TextNode(" text.", TextType.TEXT),
+        ]
+        self.assertEqual(text_nodes, expected)
+
+    def test_text_with_only_image(self):
+        text = "This is an ![image](https://example.com/image.jpg) in text."
+        text_nodes = text_to_textnodes(text)
+        expected = [
+            TextNode("This is an ", TextType.TEXT),
+            TextNode("image", TextType.IMAGE, "https://example.com/image.jpg"),
+            TextNode(" in text.", TextType.TEXT),
+        ]
+        self.assertEqual(text_nodes, expected)
+
+    def test_text_with_only_link(self):
+        text = "This is a [link](https://example.com) in text."
+        text_nodes = text_to_textnodes(text)
+        expected = [
+            TextNode("This is a ", TextType.TEXT),
+            TextNode("link", TextType.LINK, "https://example.com"),
+            TextNode(" in text.", TextType.TEXT),
+        ]
+        self.assertEqual(text_nodes, expected)
+
+    def test_text_with_multiple_bold_and_italic(self):
+        text = "This is **bold** and *italic* text."
+        text_nodes = text_to_textnodes(text)
+        expected = [
+            TextNode("This is ", TextType.TEXT),
+            TextNode("bold", TextType.BOLD),
+            TextNode(" and ", TextType.TEXT),
+            TextNode("italic", TextType.ITALIC),
+            TextNode(" text.", TextType.TEXT),
+        ]
+        self.assertEqual(text_nodes, expected)
+
+    def test_text_with_nested_formatting(self):
+        text = "This is **bold and *italic* text**."
+        text_nodes = text_to_textnodes(text)
+        expected = [
+            TextNode("This is ", TextType.TEXT),
+            TextNode("bold and ", TextType.BOLD),
+            TextNode("italic", TextType.ITALIC),
+            TextNode(" text", TextType.BOLD),
+            TextNode(".", TextType.TEXT),
+        ]
+        self.assertEqual(text_nodes, expected)
+
+    def test_text_with_multiple_images_and_links(self):
+        text = "This is text with an ![image1](https://example.com/image1.jpg) and a [link1](https://example.com/link1) and another ![image2](https://example.com/image2.jpg) and another [link2](https://example.com/link2)."
+        text_nodes = text_to_textnodes(text)
+        expected = [
+            TextNode("This is text with an ", TextType.TEXT),
+            TextNode("image1", TextType.IMAGE, "https://example.com/image1.jpg"),
+            TextNode(" and a ", TextType.TEXT),
+            TextNode("link1", TextType.LINK, "https://example.com/link1"),
+            TextNode(" and another ", TextType.TEXT),
+            TextNode("image2", TextType.IMAGE, "https://example.com/image2.jpg"),
+            TextNode(" and another ", TextType.TEXT),
+            TextNode("link2", TextType.LINK, "https://example.com/link2"),
+            TextNode(".", TextType.TEXT),
+        ]
+        self.assertEqual(text_nodes, expected)
+
+    def test_text_with_empty_string(self):
+        text = ""
+        text_nodes = text_to_textnodes(text)
+        expected = [TextNode("", TextType.TEXT)]
+        self.assertEqual(text_nodes, expected)
+
+        
 if __name__ == '__main__':
     unittest.main()
